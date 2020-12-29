b948b26817e401aed74b014b1296b610e3454e50
vivaan1209 <74779532+vivaan1209@users.noreply.github.com>
2020-12-29 15:56:40 +0530

    Add files via upload

---
+++ collisionAlgorithm-master/sketch.js
@@ -0,0 +1,37 @@
+var fixedRect, movingRect;
+var gameObject1, gameObject2
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  gameObject1 = createSprite(100,100,50,50);
+  gameObject1.shapeColor = "green";
+  gameObject1.debug = true;
+  gameObject2 = createSprite(200,100,50,50);
+  gameObject2.shapeColor = "green";
+  gameObject2.debug = true;
+}
+
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(isTouching(movingRect, gameObject1)){
+    movingRect.shapeColor = "red";
+    gameObject1.shapeColor = "red";
+  }
+else{
+  movingRect.shapeColor = "green";
+  gameObject1.shapeColor = "green";
+}
+  
+  drawSprites();
+}
